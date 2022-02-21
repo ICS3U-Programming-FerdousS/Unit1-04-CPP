937af69c6588496c7297660252e41863596797a8
Ubuntu <ubuntu@ip-172-31-46-166.ec2.internal>
2022-02-21 04:38:38 +0000

    added code for rectAngle cpp

---
+++ areaPerRect.cpp
@@ -0,0 +1,15 @@
+// Copyright (c) 2021 Ms Raffin All rights reserved.
+// Created by: Ferdous Sediqi
+// Created on: Apr. 26, 2021
+// This program calculates and displays the area and
+// perimeter of a rectangle with dimensions 5cm X 3cm.
+#include <iostream>
+
+int main() {
+  std::cout << "For a rectangle that has dimensions\n";
+  std::cout << "length 5cm and width 7cm:\n";
+  std::cout << "\n";
+  std::cout << "Area = " << 5 * 7 << " cm^2\n";
+  std::cout << "Perimeter = " << 2* (5 + 7) << " cm\n";
+}
+
---
+++ basicMath.cpp
@@ -0,0 +1,16 @@
+// Copyright (c) 2021 Ms Raffin All rights reserved.
+// Created by: Ms Raffin
+// Created on: Feb. 20, 2022
+// This program does basic math.
+#include <iostream>
+#include <cmath>
+
+int main() {
+  std::cout << "4 + 5 = " << 4+5 << std::endl;
+  std::cout << "7 - 5 = " << 7-5 << std::endl;
+  std::cout << "14 / 3 = " <<float(14)/float(3) << std::endl;
+  std::cout << "8 + 4/ 6 = " << 8+4/6 << std::endl;
+  std::cout << "3^5 = " << pow(3, 5) << std::endl;
+  std::cout << "13 / 2 = " << float(13)/float(2) << std::endl;
+  std::cout << "The square root of 49 is = " << sqrt(49) << std::endl;
+}
